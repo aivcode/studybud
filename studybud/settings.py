a3147e5696552690d949c4be4251158c0565ec6a
aiv.code <aiv.email@yandex.ru>
2022-03-22 21:49:01 +0300

    update home view, gitignore and hide secret key

--- studybud/settings.py
+++ studybud/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--4y7*-5z+pp0y@e#=66w6!%(p47usc#!9x11&^fr)ej+u))*mx'
+SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
